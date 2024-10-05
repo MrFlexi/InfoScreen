1ed7fc58307c4c14187999fcd4ad9bae02229f1a
MrFlexi <szaroletta@hotmail.com>
2024-10-05 16:10:47 +0200

    Multi Photos

--- src/app/add-dataset/add-dataset.page.ts
+++ src/app/add-dataset/add-dataset.page.ts
@@ -1,9 +1,10 @@
 // src/app/add-dataset/add-dataset.page.ts
 import { Component } from '@angular/core';
-import { Dataset } from '../models/dataset.model';  // Import the dataset interface
+import { Dataset, ty_Photo } from '../models/dataset.model';  // Import the dataset interface
 import { DataService } from '../services/data.service';
 import { PhotoService } from '../services/photo.service';
 import { NavController } from '@ionic/angular';
+import { Photo } from '@capacitor/camera';
 
 @Component({
   selector: 'app-add-dataset',
@@ -11,6 +12,10 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./add-dataset.page.scss'],
 })
 export class AddDatasetPage {
+
+  photo_array: ty_Photo[] = [];
+
+
   dataset: Dataset = {
     id: '',           // Will generate a unique ID later
     name: '',
@@ -19,8 +24,10 @@ export class AddDatasetPage {
     filepath: '',
     webviewPath: '',
     createdBy: '',
-    createdAt: ''
+    createdAt: '',
+    photos:[]
   };
+
   pictureUrl: string | undefined;
 
   constructor(
@@ -30,15 +37,37 @@ export class AddDatasetPage {
   ) {}
 
   async takePicture() {
+
+    var photo_single: ty_Photo = {    
+      name: '',
+      description: '',
+      picture: '',
+      filepath: '',
+      webviewPath: '',
+      createdBy: '',
+      createdAt: '',
+    }
+
     const newPicture = await this.photoService.addNewToGallery();
-    this.dataset.webviewPath = newPicture.webviewPath;
-    this.dataset.filepath = newPicture.filepath;    
+
+    if ( this.photo_array.length < 1) {                     // Show first picture on list
+      console.log("First entry")
+      this.dataset.webviewPath = newPicture.webviewPath;
+      this.dataset.filepath = newPicture.filepath; 
+    }    
+
+    photo_single.webviewPath = newPicture.webviewPath;
+    photo_single.filepath = newPicture.filepath;    
+    this.photo_array.push(photo_single)
+    console.log("Array lenght"+this.photo_array.length)
+
   }
 
   async saveDataset() {
     const timestamp = new Date().toISOString();
     this.dataset.id = new Date().getTime().toString();  // Generate unique ID based on current time
-    this.dataset.createdAt = timestamp;    
+    this.dataset.createdAt = timestamp;
+    this.dataset.photos = this.photo_array;    
     await this.dataService.addDataset(this.dataset);
     this.navCtrl.navigateBack('/tabs/tab1');
   }
--- src/app/models/dataset.model.ts
+++ src/app/models/dataset.model.ts
@@ -1,4 +1,16 @@
 // src/app/models/dataset.model.ts
+
+export interface ty_Photo {  
+  name: string;          // Name or title of the dataset
+  description: string;   // Description of the dataset
+  picture: string;       // Relative path or URL of the picture
+  filepath: string;
+  webviewPath?: string;
+  createdBy: string;     // The creator of the dataset
+  createdAt: string;     // Timestamp of creation (ISO 8601 format)
+}
+
+
 export interface Dataset {
     id: string;            // Unique identifier for the dataset
     name: string;          // Name or title of the dataset
@@ -8,5 +20,6 @@ export interface Dataset {
     webviewPath?: string;
     createdBy: string;     // The creator of the dataset
     createdAt: string;     // Timestamp of creation (ISO 8601 format)
+    photos?: ty_Photo[];
   }
   
