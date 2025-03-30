c45772cb439311d5a907208efb025c4225724ee4
MrFlexi <szaroletta@hotmail.com>
2025-03-30 14:33:25 +0200

    Modal klappt aber nur ohne constructor

--- src/app/ModalPopups/settings/settings.component.ts
+++ src/app/ModalPopups/settings/settings.component.ts
@@ -2,22 +2,29 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import {
- 
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonTitle,
+  IonToolbar,
+  ModalController,
 } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
   standalone: true,
+  imports: [FormsModule, IonButton, IonButtons, IonContent, IonHeader, IonItem, IonTitle, IonToolbar],
 })
 export class SettingsComponent{
 
+  name!: string;
+
   constructor() {}
-
-  closeModal() {
-  }
-
 
 
 }
